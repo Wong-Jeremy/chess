b59a022b11aad0f1a44399408137d8bd0443ac93
Jeremy Wong <jeremwong@outlook.com>
2024-11-25 17:29:36 -0800

    Refactor to use same function for Bishop, Rook, and eventually Queen.

--- src/bishop.py
+++ src/bishop.py
@@ -1,6 +1,5 @@
 """ This module implements the Bishop piece """
 
-from src.board import Board
 from src.piece import BasicPiece
 
 class Bishop(BasicPiece):
@@ -17,58 +16,14 @@ class Bishop(BasicPiece):
         self.xray_squares = []
         self.squares_defended = []
 
-        def check_direction(file, rank):
-            is_xray = False
-            potential_pinned_piece = None
-            # For backtracking x_ray squares
-            num_moves = 0
-
-            check_square = (self.pos[0] + file, self.pos[1] + rank)
-            while Board.is_valid_square(check_square):
-                self.squares_defended.append(check_square)
-                piece_at_square = board.get_piece_from_position(check_square)
-                if not board.get_piece_from_position(check_square):
-                    self.all_moves.append((check_square, 'N'))
-                    num_moves += 1
-                    check_square = (check_square[0] + file, check_square[1] + rank)
-                elif piece_at_square.get_colour() == self.get_colour():
-                    break
-                elif piece_at_square.get_colour() != self.get_colour():
-                    self.all_moves.append((check_square, 'C'))
-                    num_moves += 1
-                    potential_pinned_piece = piece_at_square
-                    is_xray = True
-                    break
-
-            # Exists to speed up pinned piece checks
-            if is_xray:
-                potential_pinned_squares = []
-                reverse_pinned_squares = [l[0] for l in self.all_moves[-num_moves:-1]]
-                reverse_pinned_squares.append(self.pos)
-                check_square = (check_square[0] + file, check_square[1] + rank)
-                while Board.is_valid_square(check_square):
-                    piece_at_square = board.get_piece_from_position(check_square)
-                    if not piece_at_square:
-                        potential_pinned_squares.append(check_square)
-                    elif piece_at_square.get_colour() == self.get_colour():
-                        break
-                    elif piece_at_square.get_colour() != self.get_colour():
-                        if piece_at_square.get_piece_type() == 'king':
-                            potential_pinned_piece.is_pinned = True
-                            potential_pinned_squares.extend(reverse_pinned_squares)
-                            potential_pinned_piece.pinned_squares = potential_pinned_squares[:]
-                        break
-                    check_square = (check_square[0] + file, check_square[1] + rank)
-
-
         # Check upper left
-        check_direction(-1,  1)
+        self.check_direction(-1,  1, board)
         # Check upper right
-        check_direction( 1,  1)
+        self.check_direction( 1,  1, board)
         # Check lower left
-        check_direction(-1, -1)
+        self.check_direction(-1, -1, board)
         # Check lower right
-        check_direction( 1, -1)
+        self.check_direction( 1, -1, board)
 
 
 
--- src/piece.py
+++ src/piece.py
@@ -86,7 +86,7 @@ class BasicPiece():
         Moves the piece
 
         Parameters:
-        pos (tuple): pos (tuple): square on the board in (rank, file) format
+        pos (tuple): square on the board in (rank, file) format
         board (Board class): the current board
 
         Returns:
@@ -113,6 +113,65 @@ class BasicPiece():
         raise NotImplementedError("Subclass needs to define this.")
 
 
+
+    def check_direction(self, file, rank, board):
+        """
+        Check available moves along a certain direction,
+        specified by `file` and `rank` directions.
+        Sets the all_moves field in this object.
+
+        Parameters:
+        file (int): +1 for increasing file, or -1 for decreasing file
+
+        rank (int): +1 for increasing file, or -1 for decreasing file
+
+        board (object): current board state
+
+        Returns:
+        None
+
+        """
+        is_xray = False
+        potential_pinned_piece = None
+        # For backtracking x_ray squares
+        num_moves = 0
+
+        check_square = (self.pos[0] + file, self.pos[1] + rank)
+        while Board.is_valid_square(check_square):
+            self.squares_defended.append(check_square)
+            piece_at_square = board.get_piece_from_position(check_square)
+            if not board.get_piece_from_position(check_square):
+                self.all_moves.append((check_square, 'N'))
+                num_moves += 1
+                check_square = (check_square[0] + file, check_square[1] + rank)
+            elif piece_at_square.get_colour() == self.get_colour():
+                break
+            elif piece_at_square.get_colour() != self.get_colour():
+                self.all_moves.append((check_square, 'C'))
+                num_moves += 1
+                potential_pinned_piece = piece_at_square
+                is_xray = True
+                break
+
+        # Exists to speed up pinned piece checks
+        if is_xray:
+            potential_pinned_squares = []
+            reverse_pinned_squares = [l[0] for l in self.all_moves[-num_moves:-1]]
+            reverse_pinned_squares.append(self.pos)
+            check_square = (check_square[0] + file, check_square[1] + rank)
+            while Board.is_valid_square(check_square):
+                piece_at_square = board.get_piece_from_position(check_square)
+                if not piece_at_square:
+                    potential_pinned_squares.append(check_square)
+                elif piece_at_square.get_colour() == self.get_colour():
+                    break
+                elif piece_at_square.get_colour() != self.get_colour():
+                    if piece_at_square.get_piece_type() == 'king':
+                        potential_pinned_piece.is_pinned = True
+                        potential_pinned_squares.extend(reverse_pinned_squares)
+                        potential_pinned_piece.pinned_squares = potential_pinned_squares[:]
+                    break
+                check_square = (check_square[0] + file, check_square[1] + rank)
 
 
 
